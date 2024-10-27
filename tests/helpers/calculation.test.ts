3ab84ca9296a48a2233a455708ea2eff06af3f3e
Dane <dane.mckillop@gmail.com>
2024-10-28 00:54:04 +1000

    Tests for calculating TotalLoan, Principal, and MonthlyPayments. To create tests for CapitalGains and CapitalGainsTax. Add validation to MonthlyPayments for negative years and interest rate.

--- tests/helpers/calculation.test.ts
+++ tests/helpers/calculation.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from 'vitest';
 import {
     calculateInterest,
     calculateTotalLoan,
+    calculatePrincipal,
+    calculateMonthlyPayment,
     calculateCapitalGains,
     calculateCapGainsTax
 } from '../../src/helpers/calculation';
@@ -9,7 +11,7 @@ import {
 //Calculate Interest - Positive
 describe('calculateInterest', () => {
 
-    it('should correctly calculate $10000 interest', () => {
+    it('should calculate $10000 interest', () => {
         const totalLoan = 110000;
         const totalPrincipal = 100000;
 
@@ -23,7 +25,7 @@ describe('calculateInterest', () => {
 //Calculate Interest - Negative
 describe('calculateInterest', () => {
 
-    it('should correctly calculate -$10000 interest', () => {
+    it('should calculate -$10000 interest', () => {
         const totalLoan = 100000;
         const totalPrincipal = 110000;
 
@@ -37,20 +39,121 @@ describe('calculateInterest', () => {
 //Calculate Total Loan - Positive
 describe('calculateTotalLoan', () => {
 
-    it('should correctly calculate -$10000 interest', () => {
-        const totalLoan = 100000;
-        const totalPrincipal = 110000;
+    it('should calculate the $360000 total loan', () => {
+        const years = 30;
+        const monthlyPayment = 1000;
 
-        const result = calculateInterest(
-            totalPrincipal, totalLoan
+        const result = calculateTotalLoan(
+            years, monthlyPayment
         );
-        expect(result).toBe(-10000);
+        expect(result).toBe(360000);
     })
 })
 
+//Calculate Total Loan - Negative
+describe('calculateTotalLoan', () => {
+
+    it('should calculate the -$360000 total loan', () => {
+        const years = 30;
+        const monthlyPayment = -1000;
+
+        const result = calculateTotalLoan(
+            years, monthlyPayment
+        );
+        expect(result).toBe(-360000);
+    })
+})
+
+//Calculate Total Loan - Zero Years
+describe('calculateTotalLoan', () => {
+
+    it('should show $0 total loan for zero years', () => {
+        const years = 0;
+        const monthlyPayment = 1000;
+
+        const result = calculateTotalLoan(
+            years, monthlyPayment
+        );
+        expect(result).toBe(0);
+    })
+})
+
+//Calculate Total Loan - Zero Repayment
+describe('calculateTotalLoan', () => {
+
+    it('should show $0 total loan for $0 monthly payments', () => {
+        const years = 30;
+        const monthlyPayment = 0;
+
+        const result = calculateTotalLoan(
+            years, monthlyPayment
+        );
+        expect(result).toBe(0);
+    })
+})
+
+//Calculate Principal - Positive
+describe('calculatePrincipal', () => {
+    
+    it('should calculate the principal as $90000', () => {
+        const loanAmount = 100000;
+        const downPayment = 10000;
+
+        const result = calculatePrincipal(
+            loanAmount, downPayment
+        );
+        expect(result).toBe(90000);
+    })
+})
+
+//Calculate Principal - Negative
+describe('calculatePrincipal', () => {
+    
+    it('should validate principal to $0 instead of negative.', () => {
+        const loanAmount = 10000;
+        const downPayment = 100000;
+
+        const result = calculatePrincipal(
+            loanAmount, downPayment
+        );
+        expect(result).toBe(0);
+    })
+})
+
+//Calculate Monthly Payment - Positive
+describe('calculateMonthlyPayment', () => {
+
+    it('should calculate monthly payment to ', () => {
+        const principal = 100000;
+        const years = 30;
+        const interestRate = 6;
+
+        const result = calculateMonthlyPayment(
+            principal, years, interestRate
+        );
+        expect(result).toBeCloseTo(599.55052515275239459146124368448, 10);
+    })
+
+})
+
+//Calculate Monthly Payment - Negative Principal
+describe ('calculateMonthlyPayment', () => {
+    
+    it('should calculate monthly payment to ', () => {
+        const principal = -100000;
+        const years = 30;
+        const interestRate = 6;
+
+        const result = calculateMonthlyPayment(
+            principal, years, interestRate
+        );
+        expect(result).toBeCloseTo(-599.55052515275239459146124368448, 10);
+    })
+})
+//Consider validation error for negative years or interest rate.
 
 //Calculate Capital Gains Tax - Bracket 1
-describe ('calculateCapitalGainsTax', () => {
+describe('calculateCapitalGainsTax', () => {
 
     it('should have no tax for bracket $18200 or less', () => {
         const result = calculateCapGainsTax(
